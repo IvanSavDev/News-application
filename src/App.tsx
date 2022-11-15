d70d86752e391d32b9736ee8cffe23f602421863
IvanSavDev <ivan-savenko@mail.ru>
2022-11-15 20:17:09 +0300

    feat: add routing and material ui components

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,26 @@
+import { Box, Container } from '@mui/material';
 import { Route, Switch } from 'react-router-dom';
-import MainPage from './MainPage';
+import MainPage from './MainPage/MainPage';
 import NewsPage from './NewsPage';
 
 const App = () => (
-  <Switch>
-    <Route exact path="/" component={MainPage} />
-    <Route path="/news" component={NewsPage} />
-  </Switch>
+  <Box
+    sx={{
+      background: '#081827',
+      minHeight: '100vh',
+      color: 'white',
+    }}
+  >
+    <Container
+      maxWidth="sm"
+      sx={{ background: '#001e3a', minHeight: '100vh', padding: '15px' }}
+    >
+      <Switch>
+        <Route exact path="/" component={MainPage} />
+        <Route path="/news" component={NewsPage} />
+      </Switch>
+    </Container>
+  </Box>
 );
 
 export default App;
