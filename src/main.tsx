494c55840b009200861ffdf1b5675ef435aca925
IvanSavDev <ivan-savenko@mail.ru>
2022-11-17 17:26:59 +0300

    refactor: delete material ui

--- src/main.tsx
+++ src/main.tsx
@@ -1,33 +1,18 @@
 /* eslint-disable comma-dangle */
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './assets/styles.css';
-import CssBaseline from '@mui/material/CssBaseline';
-import GlobalStyles from '@mui/material/GlobalStyles';
+import './assets/styles/styles.css';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { store } from './store/index';
 import App from './App';
-import '@fontsource/roboto/300.css';
-import '@fontsource/roboto/400.css';
-import '@fontsource/roboto/500.css';
-import '@fontsource/roboto/700.css';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <BrowserRouter>
-        <GlobalStyles
-          styles={{
-            body: { color: 'white' },
-            h1: { margin: 0 },
-            h2: { margin: 0 },
-            ul: { margin: 0 },
-          }}
-        />
-        <CssBaseline />
-        <App />
-      </BrowserRouter>
-    </Provider>
-  </React.StrictMode>
+  // <React.StrictMode>
+  <Provider store={store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>
+  // </React.StrictMode>
 );
