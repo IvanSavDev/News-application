1f615ab5d4ee90fa7c96ab325adc50b9a7a2f64f
IvanSavDev <ivan-savenko@mail.ru>
2022-11-23 12:07:28 +0300

    refactor: change path

--- src/store/commentsSlice.ts
+++ src/store/commentsSlice.ts
@@ -1,6 +1,6 @@
 /* eslint-disable no-param-reassign */
 import { createSlice, PayloadAction } from '@reduxjs/toolkit';
-import { Comment } from '../types';
+import { Comment } from 'src/types';
 
 export interface CommentsState {
   entities: {
