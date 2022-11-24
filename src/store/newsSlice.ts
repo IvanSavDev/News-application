4ada1ff5e58bcc7588eed96b2c018aa107ae26e5
IvanSavDev <ivan-savenko@mail.ru>
2022-11-24 14:00:11 +0300

    feat: add news reducers in slice

--- src/store/newsSlice.ts
+++ src/store/newsSlice.ts
@@ -1,24 +1,33 @@
 /* eslint-disable no-param-reassign */
 import { createSlice, PayloadAction } from '@reduxjs/toolkit';
-import { News } from '../types';
+import { News } from '../types/types';
 
 export interface NewsState {
-  entities: News[] | [];
+  entities: {
+    [id: number]: News;
+  };
 }
 
 const initialState: NewsState = {
-  entities: [],
+  entities: {},
 };
 
 export const newsSlice = createSlice({
   name: 'news',
   initialState,
   reducers: {
-    addNews: (state, action: PayloadAction<News[]>) => {
-      state.entities = [...action.payload];
+    addNews: (state, action: PayloadAction<{ id: number; news: News }>) => {
+      const { id, news } = action.payload;
+      state.entities[id] = news;
+    },
+    addSomeNews: (state, action: PayloadAction<News[]>) => {
+      action.payload.forEach((news) => (state.entities[news.id] = news));
+    },
+    deleteAllNews: (state) => {
+      state.entities = {};
     },
   },
 });
 
-export const { addNews } = newsSlice.actions;
+export const { addNews, addSomeNews } = newsSlice.actions;
 export default newsSlice.reducer;
